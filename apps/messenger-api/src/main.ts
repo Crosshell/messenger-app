c200047bd3624f66f218e7ded09e53717f5bfbe7
Crosshell <confighell@gmail.com>
2026-01-24 16:36:05 +0200

    feat(api): Enhance app initialization with CORS, cookies, validation, and global prefix

--- apps/messenger-api/src/main.ts
+++ apps/messenger-api/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+
+  const config = app.get(ConfigService);
+
+  const clientUrl = config.getOrThrow<string>('CLIENT_URL');
+
+  app.enableCors({
+    origin: clientUrl,
+    credentials: true,
+  });
+
+  app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')));
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  const port = config.get<number>('PORT') ?? 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
