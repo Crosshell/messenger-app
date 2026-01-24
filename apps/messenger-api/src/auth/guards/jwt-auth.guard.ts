9d8c23b6c686c7115603d0c9b7ca350a8468c3b7
Crosshell <confighell@gmail.com>
2026-01-24 20:20:53 +0200

    feat(auth): Add JWT authentication guard and user decorators
    - Implemented `JwtAuthGuard` to handle authentication and token validation.
    - Added `CurrentUser` decorator to retrieve the current user from the request.
    - Introduced `Authorization` decorator to simplify guard application.

---
+++ apps/messenger-api/src/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,36 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import type { Request } from 'express';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async canActivate(ctx: ExecutionContext): Promise<boolean> {
+    const request = ctx.switchToHttp().getRequest<Request>();
+
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('Token not found');
+    }
+
+    try {
+      request['user'] = await this.jwtService.verifyAsync(token);
+    } catch {
+      throw new UnauthorizedException('Invalid token');
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
