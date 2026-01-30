68609ab0c216b48e7ffc7d5fce825c2ad0063e89
Crosshell <confighell@gmail.com>
2026-01-30 18:26:33 +0200

    feat(docker): update production configuration

--- apps/messenger-api/src/prisma/prisma.service.ts
+++ apps/messenger-api/src/prisma/prisma.service.ts
@@ -1,12 +1,17 @@
+import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaPg({
+    const pool = new Pool({
       connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     });
 
+    const adapter = new PrismaPg(pool);
     super({ adapter });
   }
 }
