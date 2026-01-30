c0c0a42cd5cdd38d370160cb4df381cdc51ee80e
Crosshell <confighell@gmail.com>
2026-01-30 20:19:16 +0200

    fix(docker): include `prisma.config.ts`

--- apps/messenger-api/src/prisma/prisma.service.ts
+++ apps/messenger-api/src/prisma/prisma.service.ts
@@ -4,11 +4,15 @@ import { PrismaClient } from '@prisma/client';
 
 export class PrismaService extends PrismaClient {
   constructor() {
+    const isProd = process.env.NODE_ENV === 'production';
+
     const pool = new Pool({
       connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      ssl: isProd
+        ? {
+            rejectUnauthorized: false,
+          }
+        : false,
     });
 
     const adapter = new PrismaPg(pool);
